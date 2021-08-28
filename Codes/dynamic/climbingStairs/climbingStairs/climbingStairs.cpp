75fdaeb23d88d8eac6d1c498cf8d20be97616ac6
jingshaojing <2638117940@qq.com>
2021-08-28 11:16:20 +0800

    leetcode70 && jianzhi10

---
+++ Codes/dynamic/climbingStairs/climbingStairs/climbingStairs.cpp
@@ -0,0 +1,37 @@
+// 青蛙跳台阶、爬楼梯问题都是这种思路
+// 剑指offer110，leetcode70
+/*
+（1）dp数组的含义就是爬上第i层楼梯有几种方式
+（2）递推公式就是dp[i] = dp[i-1] + dp[i -2]
+ (3) 初值是直接dp[1]给1，dp[2]給2，从3开始
+ (4) 遍历顺序从前往后遍历即可
+ (5) 自己递推验证
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class solution
+{
+public:
+    int climbingStairs(int n)
+    {
+        // 因为直接循环到n，所以大小为n+1
+        vector<int> dp(n + 1, 0);
+        // 这种写法时间复杂度和空间复杂度都是O(N)
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n ; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    int result = solution().climbingStairs(n);
+    cout << result << endl;
+    return 0;
+}
