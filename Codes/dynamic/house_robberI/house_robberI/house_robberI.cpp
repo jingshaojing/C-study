fc629b7961234342d036a8f04ab01b8680ed59cf
jingshaojing <2638117940@qq.com>
2021-08-31 21:39:32 +0800

    leetcode 198

--- Codes/dynamic/house_robberI/house_robberI/house_robberI.cpp
+++ Codes/dynamic/house_robberI/house_robberI/house_robberI.cpp
@@ -19,7 +19,7 @@ public:
         dp[1] = max(nums[1], nums[0]);
         for (int i = 2; i < nums.size(); i++)
         {
-            dp[i] = dp[i - 2] + nums[i];
+            dp[i] =max(dp[i - 2] + nums[i], dp[i-1]);
         }
         return dp[nums.size() - 1];
     }
